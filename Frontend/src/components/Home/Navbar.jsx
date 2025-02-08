7880b5eb2c128515fd1a8169a0ad9752c7a55df9
Harsh Kaushal <harshkaushal302@gmail.com>
2025-02-08 21:43:54 +0530

    home

--- Frontend/src/components/Home/Navbar.jsx
+++ Frontend/src/components/Home/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className='justify-between flex mt-8  '>
         <div className='pl-9 flex' >
           <h1 className='kalam-bold  text-2xl text-white border-b-0'>socio</h1>
-        <h1 className='text-amber-200   text-red-500'> X </h1>
+        <h1 className='  text-red-500'> X </h1>
         <h1 className='kalam-bold text-2xl helpfulgray'>sphere</h1></div>
         <div className='pr-7'>
           <button className='px-3 roboto helpfulgray  text-l cursor-pointer '>Login</button>
